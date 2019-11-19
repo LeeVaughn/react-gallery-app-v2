030fdd1f2b11f696381824da90fe8849a2196c30
LeeVaughn <dlvprogramming@gmail.com>
2019-11-19 15:28:57 -0800

    import react-router-dom and set up initial routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+//import packages
 import React, { Component } from 'react';
 import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// import components
 import apiKey from './config.js';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
 
-// const App = () => {
 class App extends Component {
   constructor() {
     super();
@@ -15,39 +18,35 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=beach&safe_search=1&in_gallery=&per_page=24&format=json&nojsoncallback=1`;
-    axios.get(url)
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=beach&safe_search=1&in_gallery=&per_page=24&format=json&nojsoncallback=1`)
       .then(response => {
         // handle success
         this.setState({
           photoArray: response.data.photos
         });
       })
-    .catch(error => {
-      // handle error
-      console.log('Error fetching and parsing data', error);
-    })
-    .finally(function () {
-      // always executed
-    });
-    // fetch(url)
-    //   .then(response => response.json())
-    //   .then(responseData => {
-    //     this.setState({ photoArray: responseData.photos });
-    //   })
-    //   .catch(error => {
-    //     console.log('Error fetching and parsing data', error);
-    //   });
+      .catch(error => {
+        // handle error
+        console.log('Error fetching and parsing data', error);
+      })
+      .finally(function () {
+        // always executed
+      });
   }
 
   render() {
     console.log(this.state.photoArray);
     return (
-      <div className="container">
-        <SearchForm />
-        <Nav />
-        <PhotoContainer />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <SearchForm />
+          <Nav />
+          <Route exact path="/" component={ PhotoContainer } />
+          <Route path="/surfing" component={ PhotoContainer } />
+          <Route path="/skiing" component={ PhotoContainer } />
+          <Route path="/golf" component={ PhotoContainer } />
+        </div>
+      </BrowserRouter>
     );
   }
 }
